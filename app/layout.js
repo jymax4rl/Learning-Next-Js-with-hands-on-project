c3c12caf3a896742da0eb81560cea11c0d649842
jymax4rl <116975025+jymax4rl@users.noreply.github.com>
2025-10-19 02:49:57 +0200

    runnig pages.js and layout.js with tailwindcss

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,23 @@
-import React from "react";
+// import React from "react";
+import "./globals.css";
 
-const MainLayout = ({ children }) => {
+export const metadata = {
+  title: "Learning Next.js with tailwindcss",
+  description: "this is a descritption inside of metadata variable",
+  keywords: "Next.js, tailwindcss, react, github etc...",
+};
+
+const MainLayout = () => {
   return (
     <html lang="en">
-      <body>
-        <div>{children}</div>
+      <body className="flex justify-center border-4 border-black h-dvh  ">
+        <div
+          className="flex flex-col items-center-safe justify-center space- myDiv m-auto rounded-3xl  w-1/2 h-1/2 
+          p-3 shadow-xl border-1 border-solid border-b-neutral-500"
+        >
+          <h1 className="m-2">Learning Next.js with Tailwindcss</h1>
+          <h2>this is the layout.js </h2>
+        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import React from "react";
+// import React from "react";
+import { MainLayout } from "./layout";
+
+export const metadata = {
+  title: "test",
+};
 
 const HomePage = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <MainLayout />
         <div>{children}</div>
       </body>
     </html>
